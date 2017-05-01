5700589158ad1690af701f5b1d9e5eeed530e2bc
rodman01 <email4joern@arcor.de>
2017-05-01 09:57:14 +0200

    delete show progress to fix build error
    Change-Id: I710f394dc8f48ea09084c8eebe5422e89e687ffd

--- releasetools/ota_from_target_files.py
+++ releasetools/ota_from_target_files.py
@@ -823,7 +823,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.ShowProgress(0.05, 5)
   script.WriteRawImage("/boot", "boot.img")
 
-if block_based:
+  if block_based:
     script.Print(" ")
     script.Print("Flashing Boeffla...")
     script.Print(" ")
@@ -831,10 +831,6 @@ if block_based:
                    ""+input_zip.read("SYSTEM/addon.d/Boeffla.zip"))
     script.FlashBoeffla()
     script.Print(" ")
-  script.ShowProgress(0.2, 10)
-  device_specific.FullOTA_InstallEnd()
-
-  script.ShowProgress(0.2, 10)
   device_specific.FullOTA_InstallEnd()
 
   if OPTIONS.extra_script is not None:

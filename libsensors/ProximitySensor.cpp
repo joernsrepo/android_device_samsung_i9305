12465f029d0ea696ea564b0846a3f4d2ab9620bc
rodman01 <emaol4joern@arcor.de>
2017-06-03 06:57:57 +0200

    fix build error, partly revert: ibsensors: update paths to match new kernel base
    Change-Id: Iae1b73b8d88f0f871269c18a40222ab64d964108

--- libsensors/ProximitySensor.cpp
+++ libsensors/ProximitySensor.cpp
@@ -88,19 +88,22 @@ int ProximitySensor::setDelay(int32_t handle, int64_t ns)
 }
 
 int ProximitySensor::enable(int32_t handle, int en) {
-
+    int fd;
     int flags = en ? 1 : 0;
     int err;
-    //ALOGD("%s: Enable: %i", __func__, en);
+    ALOGD("%s: Enable: %i", __func__, en);
     if (flags != mEnabled) {
-         err = sspEnable(LOGTAG, SSP_PROX, en);
-         if(err >= 0){
-             mEnabled = flags;
-             setInitialState();
+        strcpy(&input_sysfs_path[input_sysfs_path_len], "enable");
+        fd = open(input_sysfs_path, O_RDWR);
+        if (fd >= 0) {
+            write(fd, en == 1 ? "1" : "0", 2);
+            close(fd);
+            mEnabled = flags;
+            setInitialState();
+            return 0;
+        }
 
-             return 0;
-         }
-         return -1;
+        return -1;
     }
     return 0;
 }

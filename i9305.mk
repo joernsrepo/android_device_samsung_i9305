6eb62f325868c62dd0dd1ef0c429df46156ad3b7
rodman01 <email4joern@arcor.de>
2017-10-18 04:57:17 +0200

    i9305.mk: add Android 8 HAL levels
    Change-Id: I5bf40ce4f0c1f014115667a74cf8913a7c382073

--- i9305.mk
+++ i9305.mk
@@ -95,3 +95,43 @@ $(call inherit-product, vendor/samsung/i9305/i9305-vendor.mk)
 # Include common makefile
 $(call inherit-product, device/samsung/smdk4412-common/common.mk)
 $(call inherit-product, device/samsung/smdk4412-qcom-common/common.mk)
+
+# HAL
+PRODUCT_PACKAGES += \
+android.hardware.power@1.0-impl \
+android.hardware.audio@2.0-impl \
+android.hardware.audio.effect@2.0-impl \
+ndroid.hardware.audio@2.0-service \
+android.hardware.soundtrigger@2.0-impl \
+android.hardware.broadcastradio@1.0-impl \
+android.hardware.keymaster@3.0-impl \
+android.hardware.keymaster@3.0-service \
+android.hardware.bluetooth@1.0-impl \
+android.hardware.bluetooth@1.0-service \
+android.hardware.camera.provider@2.4-impl \
+android.hardware.camera.provider@2.4-service \
+camera.device@3.2-impl \
+android.hardware.light@2.0-impl \
+android.hardware.usb@1.0-service \
+android.hardware.wifi@1.0 \
+android.hardware.wifi@1.0-impl \
+android.hardware.wifi@1.0-service \
+android.hardware.vibrator@1.0-impl \
+android.hardware.drm@1.0-impl \
+wificond \
+wifilogd \
+wifiloader \
+android.hardware.gnss@1.0-impl \
+android.hardware.health@1.0-impl \
+android.hardware.gatekeeper@1.0-impl \
+android.hardware.renderscript@1.0-impl \
+android.hardware.dumpstate@1.0-service \
+android.hidl.allocator@1.0-impl
+
+# Memtrack HIDL Interfaces
+PRODUCT_PACKAGES += \
+android.hardware.memtrack@1.0-impl
+
+# Configstore
+PRODUCT_PACKAGES += \
+android.hardware.configstore@1.0-impl
